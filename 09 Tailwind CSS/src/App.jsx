f0e179aeb4018fb1791aac9e3eb4bb6f906ee6e5
Vishal Kumar <vishalkumarsharma911@gmail.com>
2026-01-04 21:27:33 +0530

    Tailwind CSS

---
+++ 09 Tailwind CSS/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+  <div className="text-2xl text-white h-screen w-full
+    bg-gray-900
+  ">
+    App
+  </div>
+);
+};
+
+export default App;
